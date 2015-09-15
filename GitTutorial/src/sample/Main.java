12873e1d919dd40a0201b26cc036baccbdd28720
declanGit <declan131313@yahoo.com>
2015-09-15 16:04:00 +0300

    cm

--- GitTutorial/src/sample/Main.java
+++ GitTutorial/src/sample/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		Message message = new Message("Eclipse simple java project.!2");
-		//mod
+		//mod 2
 		System.out.println(message.inform());
 	}
 
