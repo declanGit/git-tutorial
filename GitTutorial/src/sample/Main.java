34816e3448c4ce165d470792965a29d33a667ba4
declanGit <declanGit@users.noreply.github.com>
2015-09-15 15:49:03 +0300

    e g

--- GitTutorial/src/sample/Main.java
+++ GitTutorial/src/sample/Main.java
@@ -6,9 +6,9 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		//Eclipse
+		//Eclipse Git1
 		Message message = new Message("Eclipse simple java project.");
 		System.out.println(message.inform());
 	}
-	//Ecl 0
+	//Ecl 0 Git0
 }
