ebf8e6e43b4f04fc643582cb492f0357d49e14c5
declanGit <declan131313@yahoo.com>
2015-09-17 18:09:21 +0300

    comm again

--- GitTutorial/src/sample/Main.java
+++ GitTutorial/src/sample/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		Message message = new Message("Eclipse simple java project.!3");
-		//mod 2
+		//mod 3
 		System.out.println(message.inform());
 	}
 
